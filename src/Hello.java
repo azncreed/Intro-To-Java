54525d04bf8e880f2d93e72035d287ab554bf5e6
Sylvia Shen <azncreedii@gmail.com>
2017-08-21 09:06:08 +1000

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,17 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello Ben!");
+        int number = 25;
+        int secondNumber = 45;
+        int thirdNumber = 2;
+
+        int total = number + secondNumber + thirdNumber;
+
+        System.out.println(total);
+
+
+    }
+}
+
+
+
